7940ab48fa71d68f37e15d78297be864639a3aca
ArtemSoldatovV <solvladislav777@gmail.com>
2025-03-03 18:52:25 +0300

    Delete Order_Management_System directory

--- Order_Management_System/src/main/java/Customer/CustomerRepository.java
+++
@@ -1,8 +0,0 @@
-package Customer;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-//определяется интерфейс Spring Data repository для модели Customer.Customer
-//тут в автоматическом порядке создаются: findAll, save и прочии
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-
-}
--- Order_Management_System/src/main/java/Product/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package Product;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-//определяется интерфейс Spring Data repository для модели Customer.Customer
-//тут в автоматическом порядке создаются: findAll, save и прочии
-public interface ProductRepository extends JpaRepository<Product, Long> {
-
-}
